deba7a0f2570847b2676e8f2672bb1938b8aa406
13erdprosage <oshumonchan@mac.com>
2019-08-08 19:20:30 +0900

    can output comment
    非同期通信できたけれど、まだエラーがあるのでそれを修正する予定。
    メッセージのViewが崩れているので部分パーシャル化しないとだめかも

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,6 +1,34 @@
 $(function() {
-  $(".form__message").on("submit", function() {
-    var input = $(".form__message").val();
-    console.log(input);
-  })
-})
+  function buildHTML(comment){
+      var html = `<div class="comments__main__log">
+                    <div class="contents__main__log--user_name">
+                      ${comment.user_name}
+                    </div>
+                    <div class="contents__main__log--time">
+                      ${comment.date}
+                    </div>
+                    <div class="contents__main__log--comment">
+                      ${comment.content}
+                    </div>
+                  </div>`;
+      return html;
+  }
+  $(".new_comment").on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.comments__main__log').append(html)
+      $('.contents__main__log--comment').val('')
+    })
+  });
+});
---
+++ app/views/comments/create.json.jbuilder
@@ -0,0 +1,3 @@
+json.text       @comment.content
+json.date       @comment.created_at
+json.user_name  @comment.user.name
