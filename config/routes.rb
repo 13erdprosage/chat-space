9dddaba59a6844e1f92f4b17069e67cd4af86503
13erdprosage <oshumonchan@mac.com>
2019-08-11 17:41:33 +0900

    inclemental search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users, only: [:index, :edit, :update]
+  resources :user, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update, :show]
   resources :groups, only: [:new, :edit, :create, :update] do
     resources :comments, only: [:index, :create]
   end
