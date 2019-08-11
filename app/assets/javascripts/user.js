6fc030cdd3ee882ed329d7db19854759a2877ac9
13erdprosage <oshumonchan@mac.com>
2019-08-11 17:45:45 +0900

    add, remove function by list

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,69 +1,64 @@
 $(function() {
 
   var search_list = $("#user-search-result");
-  var member_list = $("#member_search_result");
 
-  function appendUsers(user) {
-    var html =`<div class="chat-group-user clearfix js-chat-member">
-                <div class="chat-group-form__field--right">
-                <p class="chat-group-user__name">${ user.name }</p>
-                <a class="user_search_add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
-              </div>`
+  function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+                </div>`;
+      search_list.append(html);
+  }
+
+  function appendNoUser(user){
+    var html = `<div class='chat-group-user clearfix'>${ user }</div>`
     search_list.append(html);
   }
 
-  function appendMembers(name, user_id) {
-    var html =`<div class='chat-group-user clearfix js-chat-member' id="${user_id}">
-                <input name='group[user_ids][]' type='hidden' value="${ user_id }">
-                <p class='chat-group-user__name'>${ name }</p>
-                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-              </div>`
-    member_list.append(html);
-  }
+  var member_list = $("#chat-group-users");
 
-  function appendNoUsers(info) {
-    var html =`<div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">${ info }</p>
-              </div>`
-    search_list.append(html);
-  }
-  $(function() {
-    $(".chat-group-form__input").on("keyup", function() {
-      var input = $("#user-search-field").val();
-      console.log(input);
-      $.ajax({
-        type: 'GET',
-        url: '/users',
-        data: { keyword: input },
-        dataType: 'json'
-      })
-      .done(function(users) {
-        $("#user-search-result").empty();
-          if (users.length !== 0) {
-            users.forEach(function(user){
-              appendUsers(user);
-            });
-          }
-          else {
-            appendNoUsers("一致するユーザーはいません");
-          }
-        })
-      // .fail(funciton() {
-      //   alert('ユーザー検索に失敗しました');
-      // })
-    });
+  function addUser(userId,userName) {
+  var html = `<div id='chat-group-users'>
+                <div class='chat-group-user clearfix js-chat-member' id='${userId}'>
+                  <input name='group[user_ids][]' type='hidden' value='${userId}'>
+                    <p class='chat-group-user__name'>${userName}</p>
+                    <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+              </div>`;
+  member_list.append(html);
+    }
+
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(users){
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendNoUser("一致するユーザはいません");
+      }
+    })
+    .fail(function(){
+      alert('検索に失敗しました');
+    })
+  });
+  $(document).on("click", ".user-search-add", function () {
+  $('#chat-group-users').val();
+    var userId = $(this).data('user-id');
+    var userName = $(this).data('user-name');
+    addUser(userId,userName);
+    $(this).parent().remove();
   });
 
-  $(function() {
-    $(document).on('click', '.user-search-add', function() {
-      var name = $(this).attr("data-user-name");
-      var user_id = $(this).attr("data-user-id");
-      $(this).parent().remove();
-      appendMembers(name, user_id);
-    });
-
-    $(document).on("click", '.user-search-remove', function() {
-      $(this).parent().remove();
-    });
+  $(document).on("click", ".user-search-remove", function () {
+    $(this).parent().remove();
   });
 });
